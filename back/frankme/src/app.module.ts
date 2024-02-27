233cc505e8dff91d9b02ff754a7bbe20d2783f57
Manel BELHADJ <belhaj343@gmail.com>
2024-02-27 14:44:12 +0100

    added init ressources

--- back/frankme/src/app.module.ts
+++ back/frankme/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import * as process from "process";
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }),UsersModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.username,
+    password: process.env.password,
+    database: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
