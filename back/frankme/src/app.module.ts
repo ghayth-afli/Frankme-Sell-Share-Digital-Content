8e28fb4d546dc44f7beac8564036d2ac478d72e1
Manel BELHADJ <belhaj343@gmail.com>
2024-02-27 17:50:19 +0100

    Updated iam module

--- back/frankme/src/app.module.ts
+++ back/frankme/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule} from "@nestjs/config";
+import { IamModule } from './iam/iam.module';
 import * as process from "process";
 
 @Module({
@@ -18,7 +19,7 @@ import * as process from "process";
     database: 'postgres',
     autoLoadEntities: true,
     synchronize: true
-  })],
+  }), IamModule],
   controllers: [AppController],
   providers: [AppService],
 })
