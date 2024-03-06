58d0c7d7554b29a4b8f698bac719f5f435cb7fe8
Manel BELHADJ <belhaj343@gmail.com>
2024-03-06 21:25:12 +0100

    Refactor app.controller and app.module

--- back/frankme/src/app.module.ts
+++ back/frankme/src/app.module.ts
@@ -1,17 +1,16 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { IamModule } from "./iam/iam.module";
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UsersModule,
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -27,6 +26,7 @@ import { IamModule } from "./iam/iam.module";
       }),
     }),
     IamModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
