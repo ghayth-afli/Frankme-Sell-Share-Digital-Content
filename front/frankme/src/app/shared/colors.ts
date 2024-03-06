20f7f4a3a03cd8d511925751c4fa8102b05f939c
oussamaB14 <88203283+oussamaB14@users.noreply.github.com>
2024-03-06 17:01:14 +0100

    Setting up project with tailwind css

---
+++ front/frankme/src/app/shared/colors.ts
@@ -0,0 +1,9 @@
+export class MyColors {
+  PRIMARY_COLOR = '#3f51b5';
+  SECONDARY_COLOR = '#f44336';
+  ACCENT_COLOR = '#ffeb3b';
+  WARNING_COLOR = '#ffc107';
+  ERROR_COLOR = '#f44336';
+  INFO_COLOR = '#2196f3';
+  SUCCESS_COLOR = '#4caf50';
+}
