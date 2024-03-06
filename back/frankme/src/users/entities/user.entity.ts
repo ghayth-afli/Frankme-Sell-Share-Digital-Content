c2a6f25e68e1c830acfdf3803c5023f59f05b34d
Manel BELHADJ <belhaj343@gmail.com>
2024-03-06 22:00:22 +0100

    Add TypeOrmModule import and User entity to users module

--- back/frankme/src/users/entities/user.entity.ts
+++ back/frankme/src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
