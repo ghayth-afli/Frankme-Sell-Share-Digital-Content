884f7fb3ca79c4cb6a9967c2e52072f18de5f501
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-03-14 14:41:39 +0100

    add sign up form validators

--- front/src/app/core/authentication/components/register/register.component.ts
+++ front/src/app/core/authentication/components/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,21 +17,55 @@ export class RegisterComponent implements OnInit {
 
   public signupForm: FormGroup;
 
+  // Initialize the form
   ngOnInit() {
-    this.signupForm = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
-      phoneNumber: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required]),
-    });
+    this.signupForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.email]),
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        phoneNumber: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      },
+      {
+        validators: [
+          this.passwordMatchValidator as ValidatorFn,
+          this.passwordValidator as ValidatorFn,
+        ],
+      }
+    );
+  }
+
+  // Custom validator function to check if passwords match
+  passwordMatchValidator(formGroup: FormGroup) {
+    const passwordControl = formGroup.get('password');
+    const confirmPasswordControl = formGroup.get('confirmPassword');
+
+    if (passwordControl?.value !== confirmPasswordControl?.value) {
+      confirmPasswordControl?.setErrors({ passwordMismatch: true });
+    } else {
+      confirmPasswordControl?.setErrors(null);
+    }
+  }
+
+  // Custom password validator
+  passwordValidator(control: FormControl) {
+    const passwordControl = control.get('password');
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{6,}$/;
+    if (!passwordRegex.test(passwordControl?.value)) {
+      passwordControl?.setErrors({ invalidPassword: true });
+    } else {
+      passwordControl?.setErrors(null);
+    }
   }
 
   onSubmit() {
-    console.log(this.signupForm.value);
+    console.log(this.signupForm);
   }
 
+  // Hide the register form
   hideForm() {
     this.hideRegister.emit({ r: true });
   }
