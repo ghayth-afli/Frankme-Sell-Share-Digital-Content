2a1354886aeeeac61dc92ba4ec3e203949606c81
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-04-04 15:46:18 +0100

    import SharedModule

--- front/src/app/modules/user/user.module.ts
+++ front/src/app/modules/user/user.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [UserComponent],
-  imports: [CommonModule, UserRoutingModule, DashboardModule],
+  imports: [CommonModule, UserRoutingModule, DashboardModule, SharedModule],
 })
 export class UserModule {}
