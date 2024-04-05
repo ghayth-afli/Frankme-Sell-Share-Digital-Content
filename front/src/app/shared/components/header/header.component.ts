e9980bd4880fe9109489220a95085218fc20b657
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2024-04-05 14:31:15 +0100

    create header component

--- front/src/app/shared/components/header/header.component.ts
+++ front/src/app/shared/components/header/header.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
